512fd83b5d84dd746aeda63fc83afed0b70dd0cf
Emilio Fajín Bargo <emiliofbb@gmail.com>
2024-07-13 11:39:45 +0200

    Day 1 completed(First solution)

--- day1.go
+++ day1.go
@@ -16,12 +16,26 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	sum := 0
 	for scanner.Scan() {
+		var numeros []int
+		var num_sumar int
 		token := scanner.Text()
 		for _, char := range token {
 			if char >= 48 && char <= 57 {
-
+				numeros = append(numeros, int(char)-48)
 			}
 		}
+		if len(numeros) == 0 {
+			num_sumar = 0
+		} else if len(numeros) == 1 {
+			num_sumar = concat_numbers(numeros[0], numeros[0])
+		} else {
+			num_sumar = concat_numbers(numeros[0], numeros[len(numeros)-1])
+		}
+		sum = sum + num_sumar
 	}
 	fmt.Println(sum)
+}
+
+func concat_numbers(num1, num2 int) int {
+	return num1*10 + num2
 }
